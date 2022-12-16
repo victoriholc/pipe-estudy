01f879ab9e517bced330465a614bdbd75edf15a0
Victor Freitas <victoriholc@hotmail.com>
2022-12-16 03:12:50 -0300

    projeto angular pipe-estudy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipe-estudy';
+
+  nome = 'Victor';
+  dataAniversario = new Date(1993, 10, 4);
+  preco = 123123.4343444;
+  troco = 0.24214;
 }
